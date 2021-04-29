c475b732e3fb0f3a4b2ffcaba9ec0e7f9f7a7a25
tom322 <tomokitomoki322@gmail.com>
2021-04-30 05:03:34 +0900

    procon archive 2021_04_30

---
+++ AOJ/ITP1/ITP1_10_A/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+#include <iomanip>
+using namespace std;
+int main(){
+    double x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    cout << fixed << setprecision(10);
+    cout << sqrt((pow(x2-(x1), 2) + pow(y2-(y1), 2))) << endl;
+}
