221b46e0ef8bfa008430d0e673b337fe7089b2d9
tom322 <tomokitomoki322@gmail.com>
2021-05-05 01:01:08 +0900

    procon archive 2021_05_05

---
+++ study/bit_calc/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n = 5;
+
+    for (int bit = 0; bit < (1 << n); bit++)
+    {
+        vector<int> S;
+        for (int i = 0; i < n; i++)
+        {
+            if (bit & (1<<i))
+            {
+                S.push_back(i);
+            }
+        }
+
+    
+        cout << bit << ": {";
+        for (int i = 0; i < (int)S.size(); i++)
+        {
+            cout << S[i] << " ";
+        }
+        cout << "}" << endl;
+    }
+}
