ad2a9368615fa88518528b16b09c9063647ea0c5
tom322 <tomokitomoki322@gmail.com>
2021-05-18 03:24:49 +0900

    procon archive 2021_05_18

---
+++ atcoder.jp/abs/ABC083_B/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+#include<vector>
+using namespace std;
+int main(){
+    int N, A, B;
+    cin >> N >> A >> B;
+
+    int ans = 0;
+    for (int i = 1; i < N + 1; i++)
+    {
+        int calc = 0;
+        int mid = i;
+        calc += (mid % 10);
+        mid /= 10;
+        calc += (mid % 10);
+        mid /= 10;
+        calc += (mid % 10);
+        mid /= 10;
+        calc += (mid % 10);
+        mid /= 10;
+        calc += (mid % 10);
+        if(calc >= A && calc <= B) ans+=i;
+        // cout << calc << endl;
+    }
+    
+    cout << ans << endl;
+}
