e360adcc485457ca7e1144f898874624385e65c1
tom322 <tomokitomoki322@gmail.com>
2021-05-30 17:02:32 +0900

    procon archive 2021_05_30

--- study/aggregation/ABC061_B/main.cpp
+++ study/aggregation/ABC061_B/main.cpp
@@ -4,5 +4,17 @@
 #include<vector>
 using namespace std;
 int main(){
+    int n, m;
+    cin >> n >> m;
+
+    int r = m * 2;
+    int t[101] = {};
+    for (int i = 0; i < r; i++)
+    {
+        int num = 0;
+        cin >> num;
+        t[num-1]++;
+    }
     
+    for(int i = 0; i < n; i++) cout << t[i] << endl;
 }
