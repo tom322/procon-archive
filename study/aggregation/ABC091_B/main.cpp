c9d06487fa7b595aad83da9ad5988b447bf7118c
tom322 <tomokitomoki322@gmail.com>
2021-06-12 20:22:41 +0900

    procon 20210612

--- study/aggregation/ABC091_B/main.cpp
+++ study/aggregation/ABC091_B/main.cpp
@@ -4,5 +4,24 @@
 #include<vector>
 using namespace std;
 int main(){
+    int n;
+    cin >> n;
+    string s[n];
+    for(int i = 0; i < n; i++) cin >> s[i];
+
+    int m;
+    cin >> m;
+    string t[m];
+    for(int i = 0; i < m; i++) cin >> t[i];
+
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int yen = 0;
+        for(int j = 0; j < n; j++) if (s[i] == s[j]) yen++;
+        for(int k = 0; k < m; k++) if (s[i] == t[k]) yen--;
+        ans = max(ans, yen);
+    }
     
+    cout << ans << endl;
 }
