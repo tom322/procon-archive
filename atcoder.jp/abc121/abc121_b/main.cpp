27482ef398529a6a069b440014943cf9e7c15cc8
tom322 <tomokitomoki322@gmail.com>
2022-05-04 16:31:35 +0900

    procon 20220504_1

--- atcoder.jp/abc121/abc121_b/main.cpp
+++ atcoder.jp/abc121/abc121_b/main.cpp
@@ -7,31 +7,20 @@ int main(){
     int n, m, c;
     cin >> n >> m >> c;
 
-    int a[n][m];
-    int b[1][m];
+    int b[m];
     int right_answer_num = 0;
 
-    for (int i = 0; i < m; i++) {
-        cin >> b[0][i];
-    }
+    for (int i = 0; i < m; i++) cin >> b[i];
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> a[i][j];
-
-        }
-
-    }
-
-    for (int i = 0; i < n; i++) {
-        int sum = 0;
+        int sum = c;
 
         for (int j = 0; j < m; j++) {
-            sum += a[i][j] * b[0][j];
+            int a;
+            cin >> a;
+            sum += a * b[j];
 
         }
-
-        sum += c;
 
         if (sum > 0) right_answer_num += 1;
 
